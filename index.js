773de79d20ede43431149465292a18a796352c46
Nitesh6387 <niteshverma6387@gmail.com>
2025-04-17 22:06:02 +0530

    mm

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const dbCoonect = require('./Config/db');
 const router = require('./Routes/router');
-const cors=require('cors')
-
+const cors = require('cors')
+const dotenv = require('dotenv')
 const app = express()
 app.use(express.json())
 app.use(cors())
+dotenv.config()
 dbCoonect()
 app.use(router)
+const PORT = process.env.PORT || 8000;
 app.listen(8000, () => {
     console.log("server Is running on port 8000");
 })
